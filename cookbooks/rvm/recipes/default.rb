f6a95916a1139205b6626747f8630bde45ee4539
Simon Rozet <simon@rozet.name>
2010-06-07 15:05:00 +0200

    split rubies installation

--- cookbooks/rvm/recipes/default.rb
+++ cookbooks/rvm/recipes/default.rb
@@ -6,9 +6,18 @@ script "rvm bootstrap" do
   code <<SH
 export HOME=/home/#{node[:owner_name]}
 cd $HOME/.rvm/src && git clone --depth 1 git://github.com/wayneeseguin/rvm.git && cd rvm && ./install
-
-# Install common rubies
-source $HOME/.rvm/scripts/rvm
-rvm install 1.8.6,1.8.7,ree,1.9.1,jruby,rbx
 SH
 end
+
+%w[1.8.6 1.8.7 ree 1.9.1 jruby rbx].each { |ruby|
+  script "rvm install #{ruby}" do
+    interpreter "bash"
+    action :run
+    user node[:owner_name]
+    code <<-SH
+      source $HOME/.rvm/scripts/rvm
+      [[ $(rvm list strings | grep #{ruby}) ]] && exit 0
+      rvm install #{ruby}
+    SH
+  end
+}
