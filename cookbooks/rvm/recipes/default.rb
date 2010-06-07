f0d87da5317a9e78269b0792e8785b93b7af2695
Simon Rozet <simon@rozet.name>
2010-06-07 15:05:57 +0200

    make rvm a simple command

--- cookbooks/rvm/recipes/default.rb
+++ cookbooks/rvm/recipes/default.rb
@@ -1,12 +1,8 @@
-script "rvm bootstrap" do
-  interpreter "bash"
-  action :run
+execute "rvm bootstrap" do
   user node[:owner_name]
   not_if { File.directory?("/home/#{node[:owner_name]}/.rvm") }
-  code <<SH
-export HOME=/home/#{node[:owner_name]}
-cd $HOME/.rvm/src && git clone --depth 1 git://github.com/wayneeseguin/rvm.git && cd rvm && ./install
-SH
+  command "git clone --depth 1 git://github.com/wayneeseguin/rvm.git" \
+    " && cd rvm && ./install"
 end
 
 %w[1.8.6 1.8.7 ree 1.9.1 jruby rbx].each { |ruby|
